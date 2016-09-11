0ffa75ce2a2991a17025a0273573150a82b359b5
박지수 <wltn000129@gmail.com>
2016-09-11 21:12:43 +0900

    User Key Create Method Added

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 var parse = require('./route/parse.js');
 
-parse.pushlog("a");
+parse.createUserKey('c3a5142c-ad30-4589-b67f-9eac3cdfab6c', '20', 'M', '1100000000', function (result) {
+    console.log(result);
+});
+
+parse.setAppKey("asdf", function (result) {
+    console.log(result);
+})
--- route/parse.js
+++ route/parse.js
@@ -2,14 +2,47 @@
  * Created by GrooshBene on 2016. 9. 11..
  */
 
+var http = require('http');
 var https = require('https');
 
-function pushlog(content){
-    console.log(content);
+function setAppKey(appkey, callback) {
+    userAppKey = appkey;
+    return callback(appkey);
 }
 
-function setAppKey(appkey) {
+function createUserKey(appKey, age, sex, location, callback) {
+    var requestOptions = {
+        "method": "POST",
+        "hostname": "apis.eatsight.com",
+        "port": null,
+        "path": "/foodinfo/1.0/users",
+        "headers": {
+            "ds-applicationkey": appKey,
+            "content-type": "application/json",
+            "cache-control": "no-cache",
+            "postman-token": "2a2c7b4c-de56-b4d0-0860-f7817ccf3f93"
+        }
+    }
 
-}
+    var req = http.request(requestOptions, function (res) {
+        var chunks = [];
 
-exports.pushlog = pushlog;
+        res.on("data", function (chunk) {
+            chunks.push(chunk);
+        });
+
+        res.on("end", function () {
+            var body = Buffer.concat(chunks);
+            var response = body.toString();
+            return callback(JSON.parse(response));
+        });
+
+    });
+
+    req.write(JSON.stringify({age: age, sex: sex, location: location}));
+    req.end();
+    console.log(age + "," + sex + "," + location);
+};
+
+exports.setAppKey = setAppKey;
+exports.createUserKey = createUserKey;
