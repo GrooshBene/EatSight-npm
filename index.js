d79c55b0be7208077c36d6104f4af4b71b0aa1a5
박지수 <wltn000129@gmail.com>
2016-09-11 13:00:28 +0900

    parse.js added

---
+++ index.js
@@ -0,0 +1,1 @@
+var parse = require('./route/parse.js');
---
+++ route/parse.js
@@ -0,0 +1,9 @@
+/**
+ * Created by GrooshBene on 2016. 9. 11..
+ */
+
+var https = require('https');
+
+exports.pushlog = function () {
+    console.log('asdf');
+}
